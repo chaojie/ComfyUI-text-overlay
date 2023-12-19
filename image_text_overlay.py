16e6cd4a0ac85e9939c7f6a48543a52dac447412
chaojie <wang.jie@teacher.eca-jca.edu.au>
2023-12-19 09:51:11 +0800

    Update image_text_overlay.py

--- image_text_overlay.py
+++ image_text_overlay.py
@@ -11,6 +11,7 @@ import time
 from datetime import datetime
 from urllib.parse import urlencode
 import shutil
+import re
 
 # region TENSOR Utilities
 def tensor2pil(image: torch.Tensor) -> List[Image.Image]:
@@ -33,9 +34,113 @@ def pil2tensor(image: Union[Image.Image, List[Image.Image]]) -> torch.Tensor:
     
     return torch.from_numpy(np.array(image).astype(np.float32) / 255.0).unsqueeze(0)
 
+class PrepareStringForSchedule:
+    #def __init__(self, device="cpu"):
+    #    self.device = device
+
+    @classmethod
+    def INPUT_TYPES(cls):
+        return {
+            "required": {
+                "texts": ("STRING",{"multiline": True, "default": "Hello"}),
+            }
+        }
+    
+    INPUT_IS_LIST = True
+    RETURN_TYPES = ("STRING", )
+    FUNCTION = "prepare_string_for_schedule"
+    CATEGORY = "image/text"
+    
+    def prepare_string_for_schedule(self, texts):
+        ret=''
+        ind=0
+        for txt in texts:
+            txt=txt.replace('"','')
+            if len(txt.split(','))>30:
+                txt=','.join(txt.split(',')[:30])
+            ret=ret+f'"{ind}":"{txt}", '
+            ind=ind+1
+        return (ret,)
+    
+class GPTTextSchedule:
+    #def __init__(self, device="cpu"):
+    #    self.device = device
+
+    @classmethod
+    def INPUT_TYPES(cls):
+        return {
+            "required": {
+                "texts": ("STRING",{"multiline": True, "default": "Hello"}),
+                "framelen": ("INT", {"default": 16}),
+            }
+        }
+
+    RETURN_TYPES = ("STRING","STRING","STRING","INT","STRING")
+    FUNCTION = "gpt_text_schedule"
+    CATEGORY = "image/text"
+    
+    def gpt_text_schedule(self, texts,framelen):
+        prompts=[]
+        captions=[]
+        plist=re.split('\.|。|\n|，|,|\?|？|；|！',texts)
+        for pitem in plist:
+            pitem=pitem.strip()
+            if pitem!='' and len(pitem)>=2:
+                headers = {
+                    'Content-Type': 'application/json',
+                    'Authorization': 'Bearer ' + os.getenv('OPENAI_API_KEY', 'sk-wJrbjKZkhDHMRe3T5a7b212eEbD3484b9d720059BeC6C2Fd'),
+                }
+
+                json_data = {
+                    'model': 'gpt-4',
+                    'messages': [
+                        {
+                            'role': 'system',
+                            'content': '从现在开始你是一个人工智能绘画提示词生成器。根据我的中文描述生成更详细的英文标签提示词，以逗号分隔。最重要的是，你只能输出不超过25个单词。',
+                        },
+                        {
+                            'role': 'user',
+                            'content': pitem,
+                        },
+                    ],
+                }
+                
+                gpttimes=0
+                while gpttimes>=0 and gpttimes<10:
+                    try:
+                        response = requests.post('https://gptapi.us/v1/chat/completions', headers=headers, json=json_data)
+                        pitemgpt=response.json()['choices'][0]['message']['content']
+                        pitemgpt=pitemgpt.replace('"','')
+                        print(pitemgpt)
+                        if len(pitemgpt.split(' '))>30:
+                            pitemgpt=' '.join(pitemgpt.split(' ')[:30])
+                        prompts.insert(len(prompts),pitemgpt.strip())
+                        captions.insert(len(captions),pitem.strip())
+                        gpttimes=-1
+                    except:
+                        gpttimes=gpttimes+1
+                if gpttimes==10:
+                    continue
+                    
+        finalprompts=''
+        finalcaptions=''
+        finalmaskprompts=''
+        frame1prompts=''
+        
+        #framelen=16
+        framecal=1
+        moveprompts=['zoomin','zoomout','zoomin','zoomout','zoomin','zoomout','zoomin','zoomout','zoomin','zoomout','zoomin','zoomout','zoomin','zoomout','zoomin','zoomout','zoomin','zoomout','zoomin','zoomout','zoomin','zoomout','zoomin','zoomout','zoomin','zoomout','zoomin','zoomout','zoomin','zoomout','zoomin','zoomout','zoomin','zoomout','zoomin','zoomout','zoomin','zoomout','zoomin','zoomout','zoomin','zoomout','zoomin','zoomout','zoomin','zoomout','zoomin','zoomout','zoomin','zoomout','zoomin','zoomout','zoomin','zoomout','zoomin','zoomout','zoomin','zoomout','zoomin','zoomout','zoomin','zoomout']
+        for i in range(0,len(prompts),1):
+            finalcaptions=finalcaptions+f'"{i}": "{captions[i]}",'
+            finalprompts=finalprompts+f'"{i*framecal*framelen}": "{prompts[i]},(Tang Dynasty style:0.5),masterpiece, best quality, highres, original, perfect lighting, extremely detailed wallpaper, (extremely detailed CG: 1.2)",'
+            frame1prompts=frame1prompts+f'"{i*framecal*1}": "{prompts[i]},(Tang Dynasty style:0.5),masterpiece, best quality, highres, original, perfect lighting, extremely detailed wallpaper, (extremely detailed CG: 1.2)",'
+            finalmaskprompts=finalmaskprompts+f'"{i*framecal*framelen}": "writing, {moveprompts[i]}",'
+            
+        return (finalprompts,finalcaptions,finalmaskprompts,len(prompts),frame1prompts,)
+    
 class ImageTextOverlay:
-    def __init__(self, device="cpu"):
-        self.device = device
+    #def __init__(self, device="cpu"):
+    #    self.device = device
     _alignments = ["left", "right", "center"]
 
     @classmethod
@@ -68,7 +173,7 @@ class ImageTextOverlay:
         outframes=[]
         ind=0
         for image in images:
-            text=texts[ind]
+            text=texts[int(ind/(len(images)/len(texts)))]
             image = 255.0 * image.cpu().numpy()
             image = Image.fromarray(np.clip(image, 0, 255).astype(np.uint8))
             frames.append(image)
@@ -96,7 +201,7 @@ class ImageTextOverlay:
             h=image.height
             #print(w)
             #print(h)
-            text_width, text_height = draw.textsize(text, font=loaded_font)
+            text_width = draw.textlength(text, font=loaded_font)#text_width, text_height = draw.textsize(text, font=loaded_font)
             if alignment == "center":
                 x = w/2-text_width/2
             elif alignment == "right":
@@ -119,8 +224,8 @@ class ImageTextOverlay:
 
 
 class Image2AudioVideo:
-    def __init__(self, device="cpu"):
-        self.device = device
+    #def __init__(self, device="cpu"):
+    #    self.device = device
 
     @classmethod
     def INPUT_TYPES(cls):
@@ -132,6 +237,7 @@ class Image2AudioVideo:
                 "baidu_skey": ("STRING", {"default": ""}),
                 "bgaudio": ("STRING", {"default": "bg.m4a"}),
                 "fps": ("INT", {"default": 8}),
+                "outfile": ("STRING", {"default": "lc_ad_"}),
             }
         }
 
@@ -139,7 +245,7 @@ class Image2AudioVideo:
     FUNCTION = "image_to_audiovideo"
     CATEGORY = "image/text"
         
-    def image_to_audiovideo(self, images, texts, baidu_akey,baidu_skey,bgaudio,fps):
+    def image_to_audiovideo(self, images, texts, baidu_akey,baidu_skey,bgaudio,fps,outfile):
         output_dir = folder_paths.get_output_directory()
         (
             full_output_folder,
@@ -147,7 +253,7 @@ class Image2AudioVideo:
             _,
             subfolder,
             _,
-        ) = folder_paths.get_save_image_path('lc_ad_', output_dir)
+        ) = folder_paths.get_save_image_path(outfile, output_dir)
         counter=1
         file3 = f"{filename}_{counter:05}.mp3"
         file4 = f"{filename}_{counter:05}.mp4"
@@ -218,7 +324,7 @@ class Image2AudioVideo:
             while soundEx==1:
                 try:
                     resp = requests.request("POST", url, headers=headers, data=payload)
-                    soundFile=f'output/lc_ad_{itxt}.mp3'
+                    soundFile=f'output/{outfile}{itxt}.mp3'
                     with open(soundFile,'wb') as f:
                         f.write(resp.content)
                     soundcaption = AudioSegment.from_file(soundFile)
@@ -269,4 +375,6 @@ class Image2AudioVideo:
 NODE_CLASS_MAPPINGS = {
     "Image Text Overlay": ImageTextOverlay,
     "Image Audio Video": Image2AudioVideo,
+    "GPT Text Schedule": GPTTextSchedule,
+    "Prepare String For Schedule": PrepareStringForSchedule,
 }
